e31437441ae6c6a7b9b05af17dfb596727cde23a
Carlos Eduardo G. Braga <137355480+cadugoncalvesb@users.noreply.github.com>
2025-08-04 15:54:54 -0300

    Ampliar capacidade de armazenamento da variável do valor final

--- exitRecord/models.py
+++ exitRecord/models.py
@@ -7,7 +7,7 @@ import math
 
 class ExitRecord(models.Model):
     exitDate = models.DateTimeField(default=now)
-    finalValue = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    finalValue = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     entryRecord = models.ForeignKey(EntryRecord, on_delete=models.CASCADE)
     paymentType = models.ForeignKey(PaymentType, on_delete=models.SET_NULL, null=True, blank=True)
 
